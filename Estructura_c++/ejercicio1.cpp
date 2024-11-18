ada25bd4a3f13dbe35a045c8a6948f3b087ac9fc
javier olmos <rjolmos@espe.edu.ec>
2024-11-18 10:59:10 -0500

    ejercicio c++ 18/11/2024

---
+++ Estructura_c++/ejercicio1.cpp
@@ -0,0 +1,14 @@
+//programa 1 estructura c++
+#include <stdio.h>
+#include <iostream>
+int main(){
+    /*
+        mi primer programa usando los lenguajes c y c++
+        realizado en el laboratorio G202
+        autor Javier Olmos
+    */
+    printf("hola mundo\n");
+    std::cout<<"estamos en c++"<<std::endl;
+    std::cout<<"c++ esta bonito";
+    return 0;
+} 
